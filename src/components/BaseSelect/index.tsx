fb1347244c500b0172dfaa72703c81d93043e0a8
poyiding <15869507101@163.com>
2020-05-12 18:21:15 +0800

    add: init project

---
+++ src/components/BaseSelect/index.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Select } from 'antd';
+import { SelectValue, SelectProps } from 'antd/lib/select';
+
+const { Option } = Select;
+
+type dataObj = {
+  [key: string]: string | number
+}
+
+interface BaseSelectProps<T> extends SelectProps<T> {
+  data: dataObj[] | dataObj,
+  dataFormat?: dataObj
+}
+
+export default function BaseSelect(props: BaseSelectProps<SelectValue>) {
+  const { data, dataFormat, ...rest } = props;
+
+  if (Array.isArray(data)) {
+    return (
+      <Select {...rest}>
+        {data.map(ele => {
+          if (dataFormat === undefined) {
+            return (
+              <Option key={ele.key} value={ele.key}>
+                {ele.value}
+              </Option>
+            );
+          }
+          return (
+            <Option key={ele[dataFormat.key]} value={ele[dataFormat.key]}>
+              {ele[dataFormat.value]}
+            </Option>
+          );
+        })}
+      </Select>
+    );
+  }
+  return (
+    <Select {...rest}>
+      {Object.keys(data).map(item => (
+        <Option key={item} value={item} >{data[item]}</Option>
+      ))}
+    </Select>
+  );
+}
