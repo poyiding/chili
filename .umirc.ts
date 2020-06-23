2934e15c2d8bbd4c8a86d978c1511d3677b94429
poyiding <15869507101@163.com>
2020-06-23 15:09:34 +0800

    chore: update logo and description

--- .umirc.ts
+++ .umirc.ts
@@ -3,6 +3,9 @@ import { defineConfig } from 'dumi';
 // more config: https://d.umijs.org/config
 export default defineConfig({
   title: 'chili',
+  description: '基于antd封装的组件和工具库',
+  favicon: '/chili.jpeg',
+  logo: '/chili.jpeg',
   extraBabelPlugins: [
     [
       'import',
